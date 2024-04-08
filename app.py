fa6ff8e703707057f4ef22a9b4577fe12d36da05
kashani <mrakashani@gmail.com>
2024-04-08 10:54:31 +0330

    hello

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 import os
+import webbrowser
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World! (mrakashani)"
-    
+    webbrowser.open('127.0.0.1')
 
-if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
+#if __name__ == '__main__':
+#    port = os.environ.get('FLASK_PORT') or 8080
+#    port = int(port)
+
 
     app.run(port=port,host='0.0.0.0')
