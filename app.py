0b9a7f471756fb1859624159d167947e325ff77a
kashani <mrakashani@gmail.com>
2024-04-08 12:02:35 +0330

    Hello

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World! (mrakashani)"
-    
-if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 443
-    port = int(port)
+@app.route("/")
+def home():
+    return print("hello, py!")
 
 
-    app.run(port=port,host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True)
