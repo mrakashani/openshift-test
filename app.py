3a7c6709e1f1f44af5777a569408ca0144b9e593
kashani <mrakashani@gmail.com>
2024-04-09 13:30:12 +0330

    hello

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def names():
 #if __name__ == '__main__':
 #    app.run()
 if __name__ == '__main__':  
-     app.run(host='0.0.0.0', debug=True, ssl_context=context)
+     app.run(host='0.0.0.0', debug=True, port=443)
